c1787f744ff5bd7dc6bd3e2ce41cb3c26a6966d0
VicMontanez <v_montanez@hotmail.com>
2020-09-14 22:17:01 -0400

    added constructor

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/models/CatalogItem.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/models/CatalogItem.java
@@ -5,7 +5,11 @@ public class CatalogItem {
     private String desc;
     private int rating;
 
-
+    public CatalogItem(String name, String desc, int rating) {
+        this.name = name;
+        this.desc = desc;
+        this.rating = rating;
+    }
 
     public String getName() {
         return name;
