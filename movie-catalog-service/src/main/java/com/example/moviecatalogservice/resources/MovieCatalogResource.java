f0c652cbc980cdd6665b196d016dbb0e6b240bf7
VicMontanez <v_montanez@hotmail.com>
2020-09-21 22:38:53 -0400

    fixed path error

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -28,7 +28,7 @@ public class MovieCatalogResource {
         );
 
        return  ratings.stream().map(rating -> {
-           Movie movie = restTemplate.getForObject("localhost:8082/movies/foo" + rating.getMovieId(), Movie.class);
+           Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
        })
                 .collect(Collectors.toList());
