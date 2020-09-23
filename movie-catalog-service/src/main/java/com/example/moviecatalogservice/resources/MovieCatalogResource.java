d906e98237cef8762ae16712e90958b080596c25
VicMontanez <v_montanez@hotmail.com>
2020-09-23 00:23:25 -0400

    moved back to rest template

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -34,14 +34,14 @@ public class MovieCatalogResource {
         );
 
        return  ratings.stream().map(rating -> {
-           //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+           Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 
-           Movie movie = webClientBuilder.build()
-                   .get()
-                   .uri("http://localhost:8082/movies/" + rating.getMovieId())
-                   .retrieve()
-                   .bodyToMono(Movie.class)
-                   .block();
+//           Movie movie = webClientBuilder.build()
+//                   .get()
+//                   .uri("http://localhost:8082/movies/" + rating.getMovieId())
+//                   .retrieve()
+//                   .bodyToMono(Movie.class)
+//                   .block();
 
            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
        })
